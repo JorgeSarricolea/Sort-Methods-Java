e876ce30d6b8c4a0d5c70487aec931411a1bb822
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-05-14 02:22:32 -0600

    QuickSort program

---
+++ QuickSort.java
@@ -0,0 +1,54 @@
+// Here's an example of a Java program that demonstrates the QuickSort algorithm to sort an array of integers:
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = { 5, 2, 8, 12, 1, 6 };
+
+        System.out.println("Before sorting:");
+        printArray(arr);
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("After sorting:");
+        printArray(arr);
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Swap arr[i+1] and arr[high] (or the pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
